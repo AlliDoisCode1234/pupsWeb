a8df6aff2b45a0d2d441f9d13c06bfacaa4abb35
Josiah Fowler <josiah.james.fowler@gmail.com>
2021-02-21 13:28:27 -0500

    auth works / passed testing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
 import { Home } from "./pages";
 import { UserContextProvider } from "./contexts/user";
-
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
-    <UserContextProvider>
-      <div className="app">
-        <Home />
-      </div>
-    </UserContextProvider>
+    // <UserContextProvider>
+    //   <div className="app">
+    //     <Home />
+    //   </div>
+    // </UserContextProvider>
+    <div className="App">
+      <header>
+        
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
+
   );
 }
 
-export default App;
+export default withAuthenticator(App);
