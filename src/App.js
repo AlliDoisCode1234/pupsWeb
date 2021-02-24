212dbf3af2058af9b586a98c55aa0cb8fb885877
Josiah Fowler <josiah.james.fowler@gmail.com>
2021-02-24 00:44:10 -0500

    graphql mutation updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import { React, useState, useEffect} from 'react';
 import "./App.css";
-
 import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+
 
 const initialFormState = { name: '', description: '' }
 
