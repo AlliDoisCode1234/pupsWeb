23e83fedf87c1ecf57ab2f97d880673e3bdbe8da
Josiah Fowler <josiah.james.fowler@gmail.com>
2021-02-24 00:15:46 -0500

    troubleshooting graphql mutation

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
+      <h1>The Pups App</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
         placeholder="Note name"
